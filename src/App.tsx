8f6a21166f5479aa3c5edfaeb8b4b9771ee49115
pouriaH9 <pouriah9@gmail.com>
2023-04-24 17:40:02 +0330

    List and Form created

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span className='text-3xl'> and </span>
-          <a
-            className="text-3xl"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div className="w-screen h-screen flex">
+      {/* form */}
+      <div className="bg-violet-800 h-full w-1/2">
+        <h1 className="text-center text-3xl my-4 text-white">Form</h1>
+        <div className="mx-auto w-4/5 h-5/6 border-t-2 border-white"></div>
+      </div>
+
+      {/* list */}
+      <div className="bg-fuchsia-800 h-full w-1/2">
+        <h1 className="text-center text-3xl my-4 text-white">List</h1>
+        <div className="mx-auto w-4/5 h-5/6 border-t-2 border-white"></div>
+      </div>
     </div>
   );
 }
