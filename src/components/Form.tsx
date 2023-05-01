595f5d31b9411fb80cf1c974231716724f85b0eb
pouriaH9 <pouriah9@gmail.com>
2023-05-01 10:57:56 +0330

    ready to add form using formik and yup

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -17,6 +17,8 @@ const Form = () => {
     <div className="bg-violet-800 h-full w-1/2">
       <h1 className="text-center text-3xl my-4 text-white">Form</h1>
       <div className="mx-auto w-4/5 border-t-2 border-white"></div>
+      {/* FORM */}
+      sad
     </div>
   );
 };
