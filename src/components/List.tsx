af22e84bb7eb62b108f3f97e18093e04ada9b84a
pouriaH9 <pouriah9@gmail.com>
2023-05-01 11:47:20 +0330

    list aded

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -3,6 +3,14 @@ const List = () => {
     <div className="bg-fuchsia-800 h-full w-1/2">
       <h1 className="text-center text-3xl my-4 text-white">List</h1>
       <div className="mx-auto w-4/5 border-t-2 border-white"></div>
+      {/* List */}
+      <div className="w-[700px] h-[200px] mx-auto mt-10 border rounded flex flex-col gap-3">
+        <div className="mt-1 ml-2 text-white">Name:</div>
+        <div className="ml-2 text-white">Last name:</div>
+        <div className="ml-2 text-white">Phone number:</div>
+        <div className="ml-2 text-white">Birthday:</div>
+        <div className="ml-2 text-white">Bio:</div>
+      </div>
     </div>
   );
 };
